76e9b64807a5f19170c8bd097cd7ba35281a6c70
matcix <matcix@matcix.com>
2024-09-16 23:18:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,19 +4,6 @@
 
 using namespace std;
 
-void getStringLength()
-{
-    int length = 0;
-    string str;
-    cout << "Enter a string: ";
-    getline(cin, str);
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] != ' ') {
-            length++;
-        }
-    }
-    cout << "Length of the string is: " << length << endl;
-}
 
 int main() {
     sendTelegram();
